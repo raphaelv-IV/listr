4a21677ddbdecc3d964dd28c17c073f51f3bf62e
daniel-katebi <55761726+daniel-katebi@users.noreply.github.com>
2021-04-22 18:23:21 -0600

    Update TextBox2.js

--- components/TextBox2.js
+++ components/TextBox2.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import { StyleSheet, View, TextInput } from "react-native";
 import Icon from "react-native-vector-icons/Entypo";
 
-function Textbox1(props) {
+function Textbox2(props) {
+  const whatStyle = (props.ios ? styles.iphoneStyle: styles.androidStyle)
   return (
+    
     <View style={[styles.container, props.style]}>
+  
       <Icon name="mail" style={styles.iconStyle}></Icon>
-      <TextInput placeholder="Email" style={styles.inputStyle}></TextInput>
-    </View>
+      <TextInput placeholder="Email" style={whatStyle}>
+      </TextInput>
+    </View >
   );
 }
 
@@ -22,7 +26,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     paddingLeft: 8
   },
-  inputStyle: {
+  androidStyle: {
     color: "#000",
     marginLeft: 15,
     paddingRight: 5,
@@ -32,8 +36,20 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: "#D9D5DC",
     paddingTop: 14,
+    paddingBottom:8
+  },
+  iphoneStyle: {
+    color: "#000",
+    marginLeft: 15,
+    paddingRight: 5,
+    fontSize: 14,
+    flex: 1,
+    lineHeight: 16,
+    borderBottomWidth: 2,
+    borderColor: "#D9D5DC",
+    paddingTop: 14,
     paddingBottom: 8
   }
 });
 
-export default Textbox1;
+export default Textbox2;
