379d1629e5353647da698697e4680c9911fac2f1
NVasquez <56171470+raphaelv-IV@users.noreply.github.com>
2021-05-05 20:02:23 -0400

    Add files via upload

---
+++ screens/SaveScreen.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { View, TextInput, Image, Button } from 'react-native';
+import firebase from 'firebase';
+
+require("firebase/firestore")
+require("firebase/firebase-storage")
+
+export default function SaveScreen(props) {
+    const [caption, setCaption] = useState(" ")
+
+
+    const uploadImage = async() => {
+        const uri = props.route.params.image;
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        const task = firebase
+        .storage()
+        .ref()
+        .child(`post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`)
+        .put(blob)
+
+        const taskProgress = snapshot => {
+            console.log(`transferred: ${snapshot.bytesTransferrd}`)
+        }
+
+        const taskCompleted = () => {
+            task.snapshot.ref.getDownloadURL().then((snapshot) => {
+                savePostData(snapshot);
+                console.log(snapshot)
+            })
+        }
+
+        const taskError = snapshot => {
+            console.log(snapshot)
+        }
+
+        task.on("state_changed", taskProgress, taskError, taskCompleted)
+    }
+
+
+    const savePostData = (getDownloadURL) => {
+        firebase.firestore()
+        .collection('posts')
+        .doc(firebase.auth().currentUser.id)
+        .collection("userPosts")
+        .add({
+            downloadURL,
+            caption,
+            creation: firebase.firestore.FieldValue.serverTimestamp()
+        }).then((function () {
+            props.navigation.popToTop()
+        }))
+    }
+    return (
+        <View style={{ flex: 1 }}>
+            <Image source={{uri: props.route.params.image }} />
+            <TextInput
+                placeholder="Caption"
+                onChangeText={(caption) => setCaption(caption)}
+            />
+            <Button title="Save" onPress={() => uploadImage()}/>
+        </View>
+    )
+}
+
