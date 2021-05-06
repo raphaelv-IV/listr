11378ab5a7c864d1aa1a5ae52be2d1d9d727ada6
NVasquez <56171470+raphaelv-IV@users.noreply.github.com>
2021-05-05 20:03:05 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,17 @@ import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainStack from './navigation/RootNav';
+import auth from "@react-native-firebase/auth";
+import {View, Text} from 'react-native';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware } from 'redux';
+import rootReducer from './components/redux/reducers';
+import thunk from 'redux-thunk';
+import AddListScreen from './screens/AddListScreen';
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+const Stack = createStackNavigator();
+
 var firebaseConfig = {
     apiKey: "AIzaSyBA8473wfKUb1AokPxuoaoAZSs5tv5i70c",
     authDomain: "listr-ecbe5.firebaseapp.com",
@@ -14,20 +25,59 @@ var firebaseConfig = {
     appId: "1:1053803065760:web:29af2fe7d51eb3a18f74c5",
     measurementId: "G-T8G6VVEM5H"
 };
-
-
 if (!firebase.apps.length) {firebase.initializeApp(firebaseConfig)};
 
 
-
-
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            loaded: false,
+        }
+    }
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (!user) {
+                this.setState({
+                    loggedIn: false,
+                    loaded: false
+                })
+            } else {
+                this.setState({
+                    loggedIn: true,
+                    loaded: true 
+                })
+        1    }
+        })
+    }
+
+
     render() {
+        const { loggedIn, loaded } = this.state;
+        if (!loaded) {
+            return (
+                <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                    <Text>Loading</Text>
+                </View>
+            )
+        }
+        if (!loggedIn) {
+            return (
+                <MainStack/>
+              
+                
+            )
+        }
         return (
-            <MainStack/>
+            <Provider store={store}>
+               <MainStack />
+            </Provider>
+            
         )
     }
 }
+
+
 
 
 
