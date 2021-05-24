0744adecb64a7b6b55851e4dd13ba5d29e302c90
NVasquez <56171470+raphaelv-IV@users.noreply.github.com>
2021-05-24 12:04:38 -0400

    Update index.js

--- components/redux/actions/index.js
+++ components/redux/actions/index.js
@@ -4,25 +4,26 @@ import { USER_POSTS_STATE_CHANGE, USERS_POSTS_STATE_CHANGE, USERS_DATA_STATE_CHA
 export function fetchUser() {
     return ((dispatch) => {
         firebase.firestore()
-        .collection("user")
+        .collection("users")
         .doc(firebase.auth().currentUser.uid)
         .get()
         .then((snapshot) => {
             if(snapshot.exists) {
                 dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.data()})
             } else {
-                console.log("does not exist")
+                alert("DOES NOT EXIST")
             }
         })
     })
 }
+
 
 export function fetchUserPosts() {
     return ((dispatch) => {
         firebase.firestore()
         .collection("posts")
         .doc(firebase.auth().currentUser.uid)
-        .collection("usersPosts")
+        .collection("userPosts")
         .orderBy("creation", "asc")
         .get()
         .then((snapshot) => {
@@ -31,33 +32,31 @@ export function fetchUserPosts() {
                 const id = doc.id;
                 return {id, ...data}
             })
-            console.log(posts)
-            dispatch({type: USER_POSTS_STATE_CHANGE, posts})
+            dispatch({ type: USER_POSTS_STATE_CHANGE, posts})
         })
+
     })
 }
 
-
-export function fetchUsersData(uid) {
-    return((dispatch, getState) => {
-        const found = getState().usersState.users.some(el => el.uid === uid)
-        if (!found) {
-            firebase.firestore()
-            .collection("user")
-            .doc(uid)
-            .get()
-            .then((snapshot) => {
-                if (snapshot.exists) {
-                    let user = snapshot.data()
-                    user.uid = snapshot.id;
-                    dispatch({type: USERS_DATA_STATE_CHANGE, user})
-                } else {
-                    console.log("does not exist")
-                }
+export function fetchUsersPosts(uid) {
+    return ((dispatch, getState) => {
+        firebase.firestore()
+        .collection("posts")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("userPosts")
+        .orderBy("creation", "asc")
+        .get()
+        .then((snapshot) => {
+            const uid = snapshot.query.EP.path.segments[1]
+            const user = getState().usersState.users.find(el => el.uid === uid)
+            let posts = snapshot.docs.map(doc => {
+                const data = doc.data();
+                const id = doc.id;
+                return {id, ...data, user}
             })
-        }
+            dispatch({ type: USERS_POSTS_STATE_CHANGE, posts})
+        })
+
     })
 }
-
-
 
