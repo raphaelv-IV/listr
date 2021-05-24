120e3ca7fc32411e3d85366c5aefd0efb2013d3e
NVasquez <56171470+raphaelv-IV@users.noreply.github.com>
2021-05-24 12:03:33 -0400

    Update AddListScreen.js

--- screens/AddListScreen.js
+++ screens/AddListScreen.js
@@ -1,14 +1,90 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import React, { useState, useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
-const AddList = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+
+export default function AddListScreen({ navigation }) {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  
+  
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+
+      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === 'granted');
+    })();
+  }, []);
+
+
+  const takePicture = async () => {
+    if(camera) {
+      const data = await camera.takePictureAsync();
+      setImage(data.uri)
+    }
+  }
+
+  const pickImage =async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaType: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1
+    });
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  if (hasCameraPermission === null || hasGalleryPermission === false) {
+    return <View />
+  }
+  if (hasCameraPermission === false || hasGalleryPermission === false) {
+    return <Text>Access Denied</Text>
+  }
+  return (
+  <View style={{ flex: 1 }}>
+      <View style={styles.camContainer}>
+        <Camera
+        ref={ref => setCamera(ref)} 
+        style={ styles.fixedRatio } 
+        type={type}
+        ratio={'1:1'}/>
+      </View>
+      <Button
+          title="Flip Image"
+          onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+          }}>
+      </Button>
+      <Button title="Take Photo" onPress={() => takePicture()}/>
+      <Button title="Gallery" onPress={() => pickImage()}/>
+      <Button title="Save" onPress={() => navigation.navigate("Save", {image})}/>
+      {image && <Image source={{ uri: image }} style={{flex : 1}} />}
+  </View>
+  );
 }
 
-export default AddList
 
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create ({
+  camContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1    
+  }
+})
