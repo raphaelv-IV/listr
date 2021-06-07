b0cc4599fce518d132c64a88fe317b14bce3e9a7
NVasquez <56171470+raphaelv-IV@users.noreply.github.com>
2021-06-07 10:26:07 -0400

    Update MessagesScreen.js

--- screens/MessagesScreen.js
+++ screens/MessagesScreen.js
@@ -1,14 +1,88 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+// @refesh reset
+import React, {useState, useEffect, useCallback} from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { GiftedChat } from 'react-native-gifted-chat'
+import { fetchUser } from '../components/redux/actions'
+import firebase from 'firebase';
+import { AsyncStorage } from 'react-native';
 
-const Messages = () => {
+var firebaseConfig = {
+    apiKey: "AIzaSyBA8473wfKUb1AokPxuoaoAZSs5tv5i70c",
+    authDomain: "listr-ecbe5.firebaseapp.com",
+    databaseURL: "https://listr-ecbe5-default-rtdb.firebaseio.com",
+    projectId: "listr-ecbe5",
+    storageBucket: "listr-ecbe5.appspot.com",
+    messagingSenderId: "1053803065760",
+    appId: "1:1053803065760:web:29af2fe7d51eb3a18f74c5",
+    measurementId: "G-T8G6VVEM5H"
+};
+if (!firebase.apps.length) {firebase.initializeApp(firebaseConfig)};
+
+const db = firebase.firestore()
+const chatRef = db.collection('messages')   
+
+function MessagesScreen() {
+    const [user, setUser] = useState(user)
+    const [name, setName] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        readUser()
+        const unsubscribe2 = onAuthStateChange(setUser);
+        const unsubscribe = chatRef.onSnapshot(querySnapshot => {
+            const messagesFirestrore = querySnapshot
+                .docChanges()
+                .filter(({type}) => type == 'added')
+                .map(({doc}) => {
+                    const message = doc.data()
+                    return { ...message, createdAt: message.createdAt.toDate()}
+                }).sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+            appendMessages(messagesFirestrore)
+        })
+        return () => unsubscribe(), unsubscribe2()
+
+    }, [])
+
+
+    const appendMessages = useCallback((messages)=> {
+        setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
+    }, [messages])
+
+
+    async function handleSend(messages) {
+        const writes = messages.map(m => chatRef.add(m))
+        await Promise.all(writes)
+    }
+
+    async function readUser() {
+        const user = await AsyncStorage.getItem('user')
+        if (user) {
+            setUser(JSON.parse(user))
+        }
+    }
+
+    function onAuthStateChange(callback) {
+        return firebase.auth().onAuthStateChanged(user => {
+          if (user) {
+            callback({loggedIn: true});
+          } else {
+            callback({loggedIn: false});
+          }
+        });
+      }
+      
+
+    if(!user) {
+        return (
+            <View>
+                <Text>SIGN IN</Text>
+            </View>
+        )   
+    }
+
     return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+        <GiftedChat messages={messages} user={user} onSend={handleSend}/> 
+    ) 
 }
 
-export default Messages
-
-const styles = StyleSheet.create({})
+export default MessagesScreen;
