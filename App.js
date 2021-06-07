ba2147776e4f2eb626d0fbe0b10af70ed9b803eb
NVasquez <56171470+raphaelv-IV@users.noreply.github.com>
2021-06-07 10:28:19 -0400

    Update App.js

--- App.js
+++ App.js
@@ -15,16 +15,6 @@ import AddListScreen from './screens/AddListScreen';
 const store = createStore(rootReducer, applyMiddleware(thunk))
 const Stack = createStackNavigator();
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBA8473wfKUb1AokPxuoaoAZSs5tv5i70c",
-    authDomain: "listr-ecbe5.firebaseapp.com",
-    databaseURL: "https://listr-ecbe5-default-rtdb.firebaseio.com",
-    projectId: "listr-ecbe5",
-    storageBucket: "listr-ecbe5.appspot.com",
-    messagingSenderId: "1053803065760",
-    appId: "1:1053803065760:web:29af2fe7d51eb3a18f74c5",
-    measurementId: "G-T8G6VVEM5H"
-};
 if (!firebase.apps.length) {firebase.initializeApp(firebaseConfig)};
 
 
