58176be249aa2ada42ff0b157e65406420b64662
NVasquez <56171470+raphaelv-IV@users.noreply.github.com>
2021-06-09 11:05:31 -0400

    Update MessagesScreen.js

--- screens/MessagesScreen.js
+++ screens/MessagesScreen.js
@@ -6,16 +6,6 @@ import { fetchUser } from '../components/redux/actions'
 import firebase from 'firebase';
 
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBA8473wfKUb1AokPxuoaoAZSs5tv5i70c",
-    authDomain: "listr-ecbe5.firebaseapp.com",
-    databaseURL: "https://listr-ecbe5-default-rtdb.firebaseio.com",
-    projectId: "listr-ecbe5",
-    storageBucket: "listr-ecbe5.appspot.com",
-    messagingSenderId: "1053803065760",
-    appId: "1:1053803065760:web:29af2fe7d51eb3a18f74c5",
-    measurementId: "G-T8G6VVEM5H"
-};
 if (!firebase.apps.length) {firebase.initializeApp(firebaseConfig)};
 
 const db = firebase.firestore()
