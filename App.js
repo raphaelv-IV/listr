5aa9f102ced9a806a2392f82279d58157eeafffd
NVasquez <56171470+raphaelv-IV@users.noreply.github.com>
2021-06-09 07:38:36 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { Apploading, Asset, Font } from 'expo';
+import React, {useState, useEffect } from 'react';
 import * as firebase from 'firebase';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import MainStack from './navigation/RootNav';
-import auth from "@react-native-firebase/auth";
-import {View, Text} from 'react-native';
+import {View, Text, ActivityIndicator} from 'react-native';
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware } from 'redux';
 import rootReducer from './components/redux/reducers';
 import thunk from 'redux-thunk';
-import AddListScreen from './screens/AddListScreen';
+
+
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
-const Stack = createStackNavigator();
 
+var firebaseConfig = {
+    apiKey: "AIzaSyBA8473wfKUb1AokPxuoaoAZSs5tv5i70c",
+    authDomain: "listr-ecbe5.firebaseapp.com",
+    databaseURL: "https://listr-ecbe5-default-rtdb.firebaseio.com",
+    projectId: "listr-ecbe5",
+    storageBucket: "listr-ecbe5.appspot.com",
+    messagingSenderId: "1053803065760",
+    appId: "1:1053803065760:web:29af2fe7d51eb3a18f74c5",
+    measurementId: "G-T8G6VVEM5H"
+};
 if (!firebase.apps.length) {firebase.initializeApp(firebaseConfig)};
-
 
 export default class App extends React.Component {
     constructor(props) {
@@ -30,7 +35,7 @@ export default class App extends React.Component {
             if (!user) {
                 this.setState({
                     loggedIn: false,
-                    loaded: false
+                    loaded: true
                 })
             } else {
                 this.setState({
@@ -47,15 +52,13 @@ export default class App extends React.Component {
         if (!loaded) {
             return (
                 <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                    <Text>Loading</Text>
+                    <ActivityIndicator size="large" color="black"/>
                 </View>
             )
         }
         if (!loggedIn) {
             return (
                 <MainStack/>
-              
-                
             )
         }
         return (
@@ -66,8 +69,6 @@ export default class App extends React.Component {
         )
     }
 }
-
-
 
 
 
